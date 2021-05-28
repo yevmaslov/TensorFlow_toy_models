84bc2320c234a17172fe90c5d2ec7afed5ce6b8c
EvgeniiMaslov <sqrt.evmaslov@gmail.com>
2021-05-28 16:48:17 +0300

    Fix repeating sentences, add top-k strategy

--- text_generation/src/character_level/models/GPT2.py
+++ text_generation/src/character_level/models/GPT2.py
@@ -12,6 +12,11 @@ def get_pretrained_model():
 def generate_text_greedy(model, start_string):
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     input_ids = tokenizer.encode(start_string, return_tensors='tf')
-    greedy_output = model.generate(input_ids, max_length=50)
+    greedy_output = model.generate(input_ids,
+                                   max_length=150,
+                                   top_k=25,
+                                   temperature=2,
+                                   no_repeat_ngram_size=3,
+                                   early_stopping=True)
     return tokenizer.decode(greedy_output[0], skip_special_tokens=True)
 
